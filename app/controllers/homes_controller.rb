e1b213d804e894ff5017470b843b3a1d54eda1ca
parvathyv <paro_v@yahoo.com>
2015-02-04 15:43:00 -0500

    env

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 require 'open-uri'
 class HomesController < ApplicationController
   def index
-    #url = "http://api.eia.gov/category/?api_key=88DB1E7BBE358836CE91CFFCB0E0F082&category_id=35"
-    url = "http://api.eia.gov/series/?api_key=88DB1E7BBE358836CE91CFFCB0E0F082&series_id=ELEC.CONS_TOT.COW-AL-98.A"
+
+    url = "http://api.eia.gov/series/?api_key=ENV['API_KEY']&series_id=STEO.SOEPCAP_US.A"
     data = JSON.parse(open(url).read)
     binding.pry
   end
