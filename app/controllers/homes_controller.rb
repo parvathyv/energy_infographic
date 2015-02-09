aa293a97a7078360cc823b65c3c1e3eca80a1f36
parvathyv <paro_v@yahoo.com>
2015-02-08 21:25:38 -0500

    added simple animation

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,14 +1,14 @@
 require 'open-uri'
 class HomesController < ApplicationController
   def index
-    tsv = []
+    @tsv = []
     url = "http://api.eia.gov/series/?api_key=88DB1E7BBE358836CE91CFFCB0E0F082&series_id=STEO.SORCBUS.Q "
     @data = JSON.parse(open(url).read)
     @data["series"].each do|arrayquar|
       arrayquar["data"].each do|info|
-         tsv << info.first+"\t" + info[1].to_s+"\r"
+         @tsv << info.first+"\t" + info[1].to_s+"\r"
       end
     end
-   binding.pry
+    @tsv
   end
 end
