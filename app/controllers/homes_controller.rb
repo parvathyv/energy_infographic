56bed8fa03cf3ce41a74bcd5036ec6ead942ea8b
parvathyv <paro_v@yahoo.com>
2015-02-09 11:17:58 -0500

    adding animations

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,14 +1,27 @@
 require 'open-uri'
+require 'CSV'
 class HomesController < ApplicationController
   def index
-    @tsv = []
+    @dataarr = []
+    @dataarr << ['Year', 'QA']
+    color_array = ['silver','gold']
+    @csv = {}
     url = "http://api.eia.gov/series/?api_key=88DB1E7BBE358836CE91CFFCB0E0F082&series_id=STEO.SORCBUS.Q "
     @data = JSON.parse(open(url).read)
     @data["series"].each do|arrayquar|
       arrayquar["data"].each do|info|
-         @tsv << info.first+"\t" + info[1].to_s+"\r"
+         @csv[info.first] = info[1].to_s
       end
     end
-    @tsv
+
+    CSV.open("myfile.csv", "w", write_headers: true,
+    headers: ["Date","Quantity"]) do |csv|
+      @csv.map do|key, value|
+        csv << [key.split('Q').first.to_i, value]
+        @dataarr << [key.split('Q').first.to_i, value.to_f]
+      end
+    end
+    @dataarr
   end
+
 end
